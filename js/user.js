512f8484f27e9cb0905f34bb9e65da09929d9381
Vlad Ciphersky <senioreighteen@yandex.ru>
2020-04-02 10:41:40 +0500

    Update user.js

--- js/user.js
+++ js/user.js
@@ -1,7 +1,7 @@
 // jshint esversion: 6
 const forms = {
     invalid: {
-        position: 'top-end',
+        position: 'center',
         type: "error",
         title: "Ошибка!",
         html: "Вы не ввели никнейм игрока, или введённый никнейм некорректен.",
